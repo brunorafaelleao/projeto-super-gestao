69917956739f324b6cd0319b3da0dabb37fe7f14
Bruno Leão <bruno.melosh@gmail.com>
2025-10-02 15:32:46 -0400

    trabalhando com as rotas

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,30 @@ use App\Http\Controllers\SobreNosController;
 //     return "Hello World";
 // });
 
-//rota principal
-Route::get('/', [PrincipalController::class, 'principal']);
+//rota principal nomeada seguindo um padrão
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
 
 //rota sobre-nos
-Route::get('/sobre', [SobreNosController::class, 'sobre']);
+Route::get('/sobre', [SobreNosController::class, 'sobre'])->name('site.sobre');
 
 //rota contato
-Route::get('/contato', [ContatoController::class, 'contato']);
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+
+//agrupamento de rotas restritas
+Route::prefix('/app')->group(function () {
+  Route::get('/clientes', function () {
+    return 'Clientes';
+  })->name('app.clientes');
+  Route::get('/fornecedores', function () {
+    return 'Fornecedores';
+  })->name('app.fornecedores');
+  Route::get('/produtos', function () {
+    return 'Produtos';
+  })->name('app.produtos');
+});
+
+
+//rota de fallback para quando a rota não existir
+Route::fallback(function () {
+  echo 'A rota acessada não existe. <a href="' . route('site.index') . '">Clique aqui</a> para ir para a página inicial';
+});
